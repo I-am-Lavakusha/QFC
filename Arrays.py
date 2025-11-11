d2ac597e3058fe146c2ce7878d5c7c4744fec579
Lavakumar H N <121848047+I-am-Lavakusha@users.noreply.github.com>
2025-11-11 17:48:38 +0530

    Update comments to use hash for section headings

---
+++ Arrays.py
@@ -0,0 +1,14 @@
+# 1.Reverse array
+def reverseArray(a):
+    return a[::-1]
+
+# 2.hourglass
+def hourglassSum(arr):
+    max1=-math.inf
+    for row in range(len(arr)-2):
+        for coloum in range(len(arr)-2):
+            a=arr[row][coloum]+arr[row][coloum+1]+arr[row][coloum+2]+arr[row+1][coloum+1]+arr[row+2][coloum]+arr[row+2][coloum+1]+arr[row+2][coloum+2]
+            if a>=max1:
+                max1=a
+    return max1
+        
