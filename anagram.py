f6442829a041f638e083a757e9bc029bfbd3c232
I-am-Lavakusha <lavakumarhn578@gmail.com>
2025-12-02 20:48:14 +0530

    updated code

--- anagram.py
+++ anagram.py
@@ -2,17 +2,32 @@ from collections import Counter
 def is_anagram(s1, s2):
   if len(s1)!=len(s2):
     return False
-  counts, countt={}, {}
-  for i in range(len(s1)):
-    counts[s1[i]]=1+counts.get(s1[i], 0)
-    counts[s2[i]]=1+countt.get(s2[i], 0)
+  s1=list(s1)
+  s2=list(s2)
+  for i in s1:
+    if i in s2:
+      s2.remove(i)
+  if not s2:
+    return True
+  else:
+    return False
 
-  for c in counts:
-    if counts[c]!=countt.get(c, 0):
-      return False
-  return True
+  # return sorted(s1)==sorted(s2)
+
+  # if len(s1)!=len(s2):
+  #   return False
+  # counts, countt={}, {}
+  # for i in range(len(s1)):
+  #   counts[s1[i]]=1+counts.get(s1[i], 0)
+  #   counts[s2[i]]=1+countt.get(s2[i], 0)
+
+  # for c in counts:
+  #   if counts[c]!=countt.get(c, 0):
+  #     return False
+  # return True
+
   # return Counter(s1)==Counter(s2)
 
-s1=input("enter the string1")
-s2=input("enter the string2")
+s1=input("enter the string1: ")
+s2=input("enter the string2: ")
 print(is_anagram(s1, s2))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-while num<10:
-  print(num)
-  num=num+1
+  return sorted(s1)==sorted(s2)
